437867423f6de4ea86117d4824e3e063f4273a07
JohnnyW10 <60486661+JohnnyW10@users.noreply.github.com>
2021-12-09 23:06:57 +0100

    Update check_triangle_test.js

--- check_triangle_test.js
+++ check_triangle_test.js
@@ -5,6 +5,8 @@ const expect = chai.expect;
 
 describe('check_traingle', () => {
   it('This function taking value from input and tell us what kind of triangle we create if ours values are equal 0 and two shorter sides is smaller than the third side do not create triangle', () => {
-    expect(check_traingle(4,4,6).equal('It is an isosceles triangle'));
+    expect(check_traingle()).equal('Wrong value!');
+    
+
   })
 })
